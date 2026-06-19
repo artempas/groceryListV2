1d80c3adcb70eeead70dfb77fffd904bd4c351d3
Artem P <pasechnik@1060.ru>
2026-06-19 13:31:27 +0300

    feat: useListEvents hook applying SSE events to the items cache

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,4 @@
 import '@testing-library/jest-dom'
+import { MockEventSource } from './tests/helpers/mock-event-source'
+
+;(globalThis as unknown as { EventSource: unknown }).EventSource = MockEventSource
